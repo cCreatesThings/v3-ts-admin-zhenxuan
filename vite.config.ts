b0d3ec1cac3262c5b36b4e93a9c0ea9a0706ed87
lijiangtao <3530812934@qq.com>
2024-11-02 20:58:33 +0800

    feat: mock接口

--- vite.config.ts
+++ vite.config.ts
@@ -5,34 +5,40 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { viteMockServe } from 'vite-plugin-mock'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-      dts: true // 自动生成类型声明文件
-    }),
-    // svg图标配置
-    createSvgIconsPlugin({
-      iconDirs: [fileURLToPath(new URL('./src/assets/icons', import.meta.url))],
-      symbolId: 'icon-[dir]-[name]'
-    })
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";'
+export default ({ command }: { command: string }) => {
+  return defineConfig({
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+        dts: true // 自动生成类型声明文件
+      }),
+      viteMockServe({
+        localEnabled: command === 'serve'
+      }),
+      // svg图标配置
+      createSvgIconsPlugin({
+        iconDirs: [fileURLToPath(new URL('./src/assets/icons', import.meta.url))],
+        symbolId: 'icon-[dir]-[name]'
+      })
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";'
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  })
+}
